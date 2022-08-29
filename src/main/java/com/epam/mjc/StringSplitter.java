fa1098176008c11b59ddd4215bba1e8147f5be34
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-08-30 00:52:54 +0400

    stage1 module2 task DONE

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,26 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        String delimiter = String.join("", delimiters);
+        StringTokenizer splitSource = new StringTokenizer(source, delimiter);
+        while (splitSource.hasMoreTokens()) {
+            result.add(splitSource.nextToken());
+        }
+        return result;
     }
 }
